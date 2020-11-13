a72aeba90c4a33e345444d7332ff12c56957eb70
Meaghan Neylon <mneylon@richmond.edu>
2020-11-13 13:38:39 -0500

    Create values to be inserted in to the table.

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Jucy Lucy", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Pope Burger", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Tattooed Moose Burger with American", TRUE);
