dd375089ac23ef7aec4170e58a6924abfd937c99
Meaghan Neylon <mneylon@richmond.edu>
2020-11-13 13:43:56 -0500

    Created table in the command line using mysql

--- db/schema.sql
+++ db/schema.sql
@@ -4,10 +4,9 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db; 
 
-
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR (225) NOT NULL,
-    devoured BOOLEAN NOT NULL
+    devoured BOOLEAN NOT NULL,
     PRIMARY KEY (id)
-); 
+);
